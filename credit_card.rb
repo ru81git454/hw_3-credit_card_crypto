acb1f8b90fa1a047362dff98ef3a7bd4c8a8b7e2
ru81git454 <ru81o454@gmail.com>
2018-03-18 21:31:54 +0800

    luhn'

--- credit_card.rb
+++ credit_card.rb
@@ -3,17 +3,24 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number=number
+    @expiration_date=expiration_date
+    @owner=owner
+    @credit_network=credit_network
   end
 
   # returns json string
   def to_json
-    {
+    {number: @number,
+    expiration_date:@expiration_date,
+    owner: @owner,
+    credit_network: @credit_network
       # TODO: setup the hash with all instance vairables to serialize into json
     }.to_json
   end
@@ -26,6 +33,7 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    JSON.parse(card_s)
   end
 
   # return a hash of the serialized credit card object
@@ -34,6 +42,9 @@ class CreditCard
     #   - Produce a hash (using default hash method) of the credit card's
     #     serialized contents.
     #   - Credit cards with identical information should produce the same hash
+    contents=Hash.new
+
+
   end
 
   # return a cryptographically secure hash
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,7 +5,48 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
     # TODO: use the integers in nums_a to validate its last check digit
+    checksum = nums_a.pop.to_i 
+    nums_a=nums_a.reverse
+    nums_a=nums_a.map.with_index{ |e, i| i.odd? ? e : e*2 }
+    nums_a=nums_a.map{|x| x>9 ? x-9 :x }
+    nums_a=nums_a.reduce{|x,y| x+y}
+    nums_a=nums_a*9
+    nums_a=nums_a%10
+
+    if nums_a-checksum==0
+      true
+    else
+      false
+    end
   end
 end
+# module LuhnValidator
+#   # Validates credit card number using Luhn Algorithm
+#   # arguments: none
+#   # assumes: a local String called 'number' exists
+#   # returns: true/false whether last digit is correct
+#   def validate_checksum
+#     nums_a = number.to_s.chars.map(&:to_i)
+
+#     # TODO: use the integers in nums_a to validate its last check digit
+#     sum = 0
+    
+#     nums_a = nums_a.each_index do |i|
+#       index_from_right = nums_a.size() - i
+#       increment = 0
+
+#       if index_from_right%2==0
+#         increment = nums_a[i] * 2
+#         if increment >9
+#           increment -= 9
+#         end
+#       else
+#         increment = nums_a[i]
+#       end
+#       sum +=  increment
+#     end
+
+#     judge = sum%10==0? true:false
+#   end
+# end
